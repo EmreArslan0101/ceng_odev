702bb7c0fe0f02683d943fa330dac16acefea740
Emre Arslan <emrearslan3939@gmail.com>
2025-05-07 23:25:02 +0300

    More comments added

--- proj/untitled/mainwindow.cpp
+++ proj/untitled/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+// This is the equivalent of Qt Application Class code of Pytoh in C++
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
