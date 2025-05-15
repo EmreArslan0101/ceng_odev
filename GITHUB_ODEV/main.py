e4083a9e6809f58c0911e1d29cb55b0593fabe55
Emre Arslan <emrearslan3939@gmail.com>
2025-05-15 22:56:34 +0300

    Some required libraries has been added

--- GITHUB_ODEV/main.py
+++ GITHUB_ODEV/main.py
@@ -1,12 +1,15 @@
 # This Python file uses the following encoding: utf-8
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import Slot
+import markdown
 
 class MainApp(QMainWindow):
 
     def __init__(self):
         super().__init__()
         self.setWindowTitle("TXT to MD Note App")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
