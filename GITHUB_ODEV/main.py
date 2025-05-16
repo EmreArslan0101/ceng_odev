117876bef14983464c703efc024d8f8dcfc2a92a
Emre Arslan <emrearslan3939@gmail.com>
2025-05-16 21:46:22 +0300

    UI file has been translated to Python for easier usage

--- GITHUB_ODEV/main.py
+++ GITHUB_ODEV/main.py
@@ -1,18 +1,17 @@
 # This Python file uses the following encoding: utf-8
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
-from PySide6.QtCore import Slot
-import markdown
+from mainui import Ui_MainWindow 
 
 class MainApp(QMainWindow):
-
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("TXT to MD Note App")
-
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainApp()
     window.show()
     sys.exit(app.exec())
+
---
+++ GITHUB_ODEV/mainui.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'mainui.ui'
+##
+## Created by: Qt User Interface Compiler version 6.9.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QMainWindow, QPlainTextEdit,
+    QSizePolicy, QStatusBar, QTextBrowser, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 500)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.horizontalLayout = QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.writeableZone = QPlainTextEdit(self.centralwidget)
+        self.writeableZone.setObjectName(u"writeableZone")
+
+        self.horizontalLayout.addWidget(self.writeableZone)
+
+        self.markdownZone = QTextBrowser(self.centralwidget)
+        self.markdownZone.setObjectName(u"markdownZone")
+
+        self.horizontalLayout.addWidget(self.markdownZone)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Markdown Edit\u00f6r", None))
+        self.writeableZone.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Buraya yaz\u0131n...", None))
+    # retranslateUi
+
