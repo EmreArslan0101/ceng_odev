2446163d94060216c2616375506a68bd10f258c2
Emre Arslan <emrearslan3939@gmail.com>
2025-05-23 20:04:10 +0300

    Final

--- GITHUB_ODEV/main.py
+++ GITHUB_ODEV/main.py
@@ -10,15 +10,32 @@ class MainApp(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        
+        # Gösterilen Markdown verisini yazdıkça güncellemek adına, 
+        # writeablezone nesnesini textChanged sinyaline bağlıyorum 
+        self.ui.writeableZone.textChanged.connect(self.update_markdown_preview)
 
-        def md_converter(self, text_content: str) -> str:
-            md_html = markdown.markdown(text_content)
-            return md_html
+        # Markdown içeriğini herhangi bir hata yaşanamamsı adına önceden siliyorum
+        self.update_markdown_preview()
+    
+    # Yazılan MD yazısını MD formatına uygun bir HTML'ye çeviriyor 
+    def md_converter(self, text_content: str) -> str:
+        md_html = markdown.markdown(text_content, extensions=['extra', 'codehilite'])
+        return md_html
+    
+    # Çevrilen HTML verisini kullanarak bir görsel sonuç ortaya çıkartıyor
+    # ya da Markdown HTML'nin yazılacağı yeri temizliyor
+    def update_markdown_preview(self):
 
+        markdown_text = self.ui.writeableZone.toPlainText()
+        
+        html_output = self.md_converter(markdown_text)
+        
+        # HTML Markdown verisini uygun biçimde markdownzone nesnesine atıyorum
+        self.ui.markdownZone.setHtml(html_output)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainApp()
     window.show()
     sys.exit(app.exec())
-
